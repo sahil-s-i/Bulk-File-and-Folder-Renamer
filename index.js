3d9e181e3814b356edcbadff9fa2e9bd81bee9f1
Sahil <sahilismailnavar@gmail.com>
2024-04-19 08:55:20 +0530

    Added preview specific code in the file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const replaceThis = "demo";
 const replaceWith = "replaceddemo";
-const preview = true
+const preview = true;
 
 try {
     fs.readdir("data", (err, data) => {
@@ -9,10 +9,13 @@ try {
         for (let index = 0; index < data.length; index++) {
             const item = data[index];
             let newFile = "data/" + item.replaceAll(replaceThis, replaceWith)
-            if (!preview) {
-                fs.rename("data/" + item, newFile, () => {
+            if(!preview){
+                fs.rename("data/" + item, newFile , ()=>{
                     console.log("Renamed Successfully");
                 })
+            }
+            else{
+                console.log("data/" + item + " Will be renamed to " + newFile );
             }
         }
     })
