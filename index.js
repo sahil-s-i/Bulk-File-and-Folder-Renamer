f2e467d8dc0e8ac795166bd57df340ee74d80d30
Sahil <sahilismailnavar@gmail.com>
2024-04-20 13:10:21 +0530

    Added folder variable and path to the code

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const path = require('path');
 const replaceThis = "demo";
 const replaceWith = "replaceddemo";
 const preview = true;
-const folder = path(__dirname)
+const folder = path.parse(__dirname)
 
 try {
     fs.readdir(folder, (err, data) => {
         console.log(data);
         for (let index = 0; index < data.length; index++) {
             const item = data[index];
-            let newFile = "data/" + item.replaceAll(replaceThis, replaceWith)
-            if(!preview){
-                fs.rename("data/" + item, newFile , ()=>{
+            let oldFile = path.join(folder, item);
+            let newFile = path.join(folder, item.replaceAll(replaceThis, replaceWith))
+            if (!preview) {
+                fs.rename(oldFile, newFile, () => {
                     console.log("Renamed Successfully");
                 })
             }
-            else{
-                console.log("data/" + item + " Will be renamed to " + newFile );
+            else {
+                console.log("data/" + item + " Will be renamed to " + newFile);
             }
         }
     })
